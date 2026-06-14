c598d60fbb89dab8b89841080f6041c7e13c5293
Chaewoniiiie <wille.steven27@gmail.com>
2026-06-14 16:46:08 +0200

    Increase minimum zoom level from 5 to 17

--- js/app.js
+++ js/app.js
@@ -156,7 +156,7 @@ function updateActiveMarker(index){
 
     map.setView(
         activeMarker.getLatLng(),
-        Math.max(map.getZoom(),5),
+        Math.max(map.getZoom(),17),
         {
             animate:true
         }
