70c14afc2d52f5e1f09e34cb9d408f25f7354416
Chaewoniiiie <wille.steven27@gmail.com>
2026-06-16 16:47:12 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const map = L.map('map').setView([36.3,127.9],7);
 
 var OpenStreetMap_DE = L.tileLayer('https://tile.openstreetmap.de/{z}/{x}/{y}.png', {
-	maxZoom: 18,
+	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
@@ -426,8 +426,8 @@ function showMedia(index) {
 
         map.flyTo(
             markers[index].getLatLng(),
-            17,
-            { duration: 1.2,easeLinearity: 0.2 }
+            19,
+            { duration: 1.5,easeLinearity: 0.2 }
         );
 
         console.log('Map center:', map.getCenter());
